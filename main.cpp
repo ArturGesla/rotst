3470cc34aebc9db9048e362c8f40e30c4c62ceeb
artur gesla <agesla09e@gmail.com>
2022-01-27 15:57:12 +0100

    commit

--- main.cpp
+++ main.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
 #include <string>
+#include <tuple>
+#include <vector>
 
-#include "rotst.hpp"
+void help_fun();
+void ver_fun();
+void lsol_fun();
+void rs_fun(int NSTEPS);
 
+std::vector<double> readInput();
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
+	std::vector<double> data = readInput();
 
-//	std::cout << "Have " << argc << " arguments:" << std::endl;
-//	for (int i = 0; i < argc; ++i) {
-//        	std::cout << argv[i] << std::endl;
-//    	}
+	int NSTEPS = data[0];
 
-	std::string help_msg=	"--help";
-	std::string ver_msg=	"--version";
-	std::string lsol_msg=	"lsol";
+	//	std::cout << "Have " << argc << " arguments:" << std::endl;
+	//	for (int i = 0; i < argc; ++i) {
+	//        	std::cout << argv[i] << std::endl;
+	//    	}
 
-	for (int i=0; i<argc; i++){
-		if(argv[i]==help_msg) 	help_fun();
-		if(argv[i]==ver_msg) 	ver_fun();
-		if(argv[i]==lsol_msg) 	lsol_fun();
+	std::string help_msg = "--help";
+	std::string ver_msg = "--version";
+	std::string lsol_msg = "lsol";
+	std::string rs_msg = "-rs";
+
+	for (int i = 0; i < argc; i++)
+	{
+		if (argv[i] == help_msg)
+			help_fun();
+		if (argv[i] == ver_msg)
+			ver_fun();
+		if (argv[i] == lsol_msg)
+			lsol_fun();
+		if (argv[i] == rs_msg)
+			rs_fun(NSTEPS);
 	}
 	return 0;
 }
--- rotst.hpp
+++ rotst.hpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <vector>
 
-void help_fun();
+Eigen::MatrixXd eval_jacobian_u(Eigen::VectorXd u, double lam, double h, std::array<double,6> bc);
+Eigen::VectorXd eval_jacobian_lam(Eigen::VectorXd u_0, double lam_0, double h, std::array<double,6> bc);
+Eigen::VectorXd eval_G(Eigen::VectorXd u_0, double lam_0, double h, std::array<double,6> bc);
+double eval_N(Eigen::VectorXd u, double lam, Eigen::VectorXd u_0, double lam_0, Eigen::VectorXd u_dot, double lam_dot, double ds);
 
-void ver_fun();
+std::pair<Eigen::MatrixXd,Eigen::VectorXd> assembly_vec(Eigen::MatrixXd J_u, Eigen::VectorXd J_lam, Eigen::VectorXd u_dot, double lam_dot);
+std::pair<Eigen::MatrixXd, Eigen::VectorXd> assembly(Eigen::MatrixXd J_u, Eigen::VectorXd J_lam, Eigen::VectorXd u_dot, double lam_dot, Eigen::VectorXd G, double N);
 
-void lsol_fun();
+std::tuple<Eigen::VectorXd,Eigen::VectorXd,double,double,double> pc_iteration_k(Eigen::VectorXd u_0, Eigen::VectorXd u_dot,double lam_0, double lam_dot, double h, std::array<double,6> bc,double ds);
+
